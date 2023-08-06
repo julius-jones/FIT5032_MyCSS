90e1742b38a24dbe6e87ce37a46a98ead7beb0e9
Julius Jones <jjon0014@student.monash.edu>
2023-08-06 20:28:23 +1000

    Add project files.

---
+++ FIT5032_MyCSS/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FIT5032_MyCSS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FIT5032_MyCSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Example()
+        {
+            ViewBag.Message = "ExamplePage";
+            return View();
+        }
+    }
+}
---
+++ FIT5032_MyCSS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIT5032_MyCSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
